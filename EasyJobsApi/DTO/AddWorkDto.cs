1bc3ee7247ca5cbe1df7843295370eec69d3ecd8
BankHz <bhznextgen1@gmail.com>
2019-10-20 19:08:24 +0700

    AddWork เพิ่มงาน
    มี Add Location , Add Status , Add Work

---
+++ EasyJobsApi/DTO/AddWorkDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasyJobsApi.DTO
+{
+    public class AddWorkDto
+    {
+        public Guid member_id { get; set; }
+        public string work_name { get; set; }
+        public string work_desc { get; set; }
+        public string tel { get; set; }
+        public int labor_cost { get; set; }
+        public DateTime duration { get; set; }
+        public DateTime datetime { get; set; }
+        public float lat { get; set; }
+        public float @long { get; set; }
+        public string loc_name { get; set; }
+
+    }
+}
